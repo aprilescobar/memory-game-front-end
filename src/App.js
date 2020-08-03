eae87b54dd827392573b6ef523741a4e9089ec25
aprilfaithdays <aprilfaith.e@gmail.com>
2020-08-03 12:48:44 -0500

    basic game logic fully functioning

--- src/App.js
+++ src/App.js
@@ -13,18 +13,18 @@ const App = () => {
   
 
   const gameDeck  = [
-    { id: 1, name:'card1', front: card1, back: cardBack, flip: true},
-    { id: 2, name:'card1', front: card1, back: cardBack, flip: true},
-    { id: 3, name:'card2', front: card2, back: cardBack, flip: true},
-    { id: 4, name:'card2', front: card2, back: cardBack, flip: true},
-    { id: 5, name:'card3', front: card3, back: cardBack, flip: true},
-    { id: 6, name:'card3', front: card3, back: cardBack, flip: true},
-    { id: 7, name:'card4', front: card4, back: cardBack, flip: true},
-    { id: 8, name:'card4', front: card4, back: cardBack, flip: true},
-    { id: 9, name:'card5', front: card5, back: cardBack, flip: true},
-    { id: 10, name:'card5', front: card5, back: cardBack, flip: true},
-    { id: 11, name:'card6', front: card6, back: cardBack, flip: true},
-    { id: 12, name:'card6', front: card6, back: cardBack, flip: true}
+    { id: 1, name:'card1', front: card1, back: cardBack, flip: false},
+    { id: 2, name:'card1', front: card1, back: cardBack, flip: false},
+    { id: 3, name:'card2', front: card2, back: cardBack, flip: false},
+    { id: 4, name:'card2', front: card2, back: cardBack, flip: false},
+    { id: 5, name:'card3', front: card3, back: cardBack, flip: false},
+    { id: 6, name:'card3', front: card3, back: cardBack, flip: false},
+    { id: 7, name:'card4', front: card4, back: cardBack, flip: false},
+    { id: 8, name:'card4', front: card4, back: cardBack, flip: false},
+    { id: 9, name:'card5', front: card5, back: cardBack, flip: false},
+    { id: 10, name:'card5', front: card5, back: cardBack, flip: false},
+    { id: 11, name:'card6', front: card6, back: cardBack, flip: false},
+    { id: 12, name:'card6', front: card6, back: cardBack, flip: false}
   ]
 
   const [deck, setDeck] = useState([])
